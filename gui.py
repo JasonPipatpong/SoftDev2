ef9474b4d542c71739bb822db8550fccf643a3ff
Pipatpong Jason Stark <58975051+PipatpongStark@users.noreply.github.com>
2019-12-25 19:09:07 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,10 +1,7 @@
 
 from tkinter import *
-#import tkinter as tk
-#from tkinter.ttk import *
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-#import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("TkAgg") 
 from matplotlib.figure import Figure
@@ -55,7 +52,6 @@ def plot_top10():
     x=[x for x,y in top]
     y=[y for x,y in top]
     text=['A','B','C','D','E','F','G','H','I','L']
-    #['L','I','H','G','F','E','D','C','B','A']
     figure1 = Figure(figsize=(6.5,6), dpi=70)
     ax1 = figure1.add_subplot(111)
     bar1 = FigureCanvasTkAgg(figure1, window)
@@ -72,16 +68,11 @@ window.title("Log Analysis")
 window.geometry('1920x700')
 s = ttk.Style(window)
 s.theme_use('clam')
-#btn_add = Button(window, text="Date" )
-#btn_add.place(x=10,y=10)
-#entry.grid(row=1,column=0,pady=2)
 menubar = Menu(window)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Open", command=open_file)
-#filemenu.add_command(label="Open", command=donothing)
 filemenu.add_command(label="Save", command=donothing)
 filemenu.add_separator()
-#filemenu.add_command(label="Exit", command=window.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 helpmenu = Menu(menubar, tearoff=0)
@@ -89,61 +80,46 @@ helpmenu.add_command(label="Help Index", command=donothing)
 helpmenu.add_command(label="About...", command=donothing)
 menubar.add_cascade(label="Help", menu=helpmenu)
 window.config(menu=menubar)
-
-
-
 def calendar_start():
     def print_sel():
         en_date1.delete(0,END)
         en_date1.insert(0,cal.get_date())
         top.destroy()
-
     top = Toplevel(window)
-
     cal = Calendar(top,date_pattern='d/m/yy',showweeknumbers=FALSE,showothermonthdays=FALSE,
                    font="Arial 14", selectmode='day',
                     year=2015, month=5, day=17)
     cal.pack(fill="both", expand=True)
     Button(top, text="ok", command=print_sel).pack()
-
 def calendar_stop():
     def print_sel():
         en_date2.delete(0,END)
         en_date2.insert(0,cal.get_date())
         top.destroy()
-
     top = Toplevel(window)
-
     cal = Calendar(top,date_pattern='d/m/yy',showweeknumbers=FALSE,showothermonthdays=FALSE,
                    font="Arial 14", selectmode='day',
                     year=2015, month=5, day=20)
     cal.pack(fill="both", expand=True)
     Button(top, text="ok", command=print_sel).pack()
-
-    
-
 label2=Label(window,text='DATE')
 label2.place(x=0,y=0)
-
 label1=Label(window,text='Start date')
 label1.place(x=0,y=20)
 en_date1=Entry(window,width=10)
 en_date1.place(x=0,y=40)
 Button(window, text='Calendar', command=calendar_start).place(x=100, y=40)
-
 label3=Label(window,text='Stop date')
 label3.place(x=0,y=80)
 en_date2=Entry(window,width=10)
 en_date2.place(x=0,y=100)
 Button(window, text='Calendar', command=calendar_stop).place(x=100, y=100)
-
 btn_all=Button(window,text='View all',command=view_all)
 btn_all.place(x=0,y=270)
 en_search=Entry(window,width=15)
 en_search.place(x=80,y=270)
 btn_search=Button(window,text='Search')
 btn_search.place(x=230,y=270)
-
 frame = Frame(window)
 box_data=Listbox(window,width=50,height=20)
 box_data.place(x=30,y=300)
@@ -151,12 +127,8 @@ frame.place(x = 490, y = 300 ,height=340)
 scrollbar = Scrollbar(frame, orient="vertical",command=box_data.yview)
 scrollbar.pack(side="right", fill="y")
 box_data.config(yscrollcommand=scrollbar.set)
-
-
 Button(window, text='Analysis', command=plot_top10).place(x=50, y=150)
 small=font.Font(size=10)
 box_grap=Listbox(window,width=30,height=12)
 box_grap.place(x=1100,y=430)
-
-
 window.mainloop()
